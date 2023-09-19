8d86b59948097979cdc5f4d0c37b881074939e92
Wilder Gonzales <wilder.gonzales@webbeds.com>
2023-09-19 13:59:38 +0200

    test waterfall workflow

--- cypress/e2e/regex.cy.js
+++ cypress/e2e/regex.cy.js
@@ -1,6 +1,6 @@
 describe('regex example', () => {
 
-  it('test should match', () => {
+  it.skip('test should match', () => {
     const url1 = '/interface/es/login/'
     const regexPattern = /\/interface\/\w+\/login\//
     cy.wrap(url1).should('match', regexPattern)
