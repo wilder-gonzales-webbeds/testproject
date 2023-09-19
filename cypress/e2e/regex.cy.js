dba371fb9ec1be982569944a167e23dc669b2a63
Wilder Gonzales <wilder.gonzales@webbeds.com>
2023-09-19 15:02:36 +0200

    push master

--- cypress/e2e/regex.cy.js
+++ cypress/e2e/regex.cy.js
@@ -1,6 +1,6 @@
 describe('regex example', () => {
 
-  it.skip('test should match', () => {
+  it('test should match', () => {
     const url1 = '/interface/es/login/'
     const regexPattern = /\/interface\/\w+\/login\//
     cy.wrap(url1).should('match', regexPattern)
