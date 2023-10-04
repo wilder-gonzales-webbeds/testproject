99ee853c9895ad28fe4b31c32779958fcf89cb99
Wilder Gonzales <wilder.gonzales@webbeds.com>
2023-10-04 16:14:55 +0200

    test extranet login

---
+++ cypress/e2e/extranet.cy.js
@@ -0,0 +1,14 @@
+Cypress.config('baseUrl', 'https://www.dotwconnect.com/_extranet')
+
+describe('login', function () {
+
+  it('login and logout', function () {
+    cy.visit('/')
+    cy.get("[placeholder='Username']").type(Cypress.env('extranetUsername'))
+    cy.get("[placeholder='Password']").type(Cypress.env('extranetPassword'), { log: false })
+    cy.get("button[type='submit']").click()
+
+    cy.get("#headerProfilePhoto").should('be.visible')
+  })
+
+})
